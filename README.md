50e70cc3e4e67cbd6afb7741dd0b376dbee35efd
Alex-BusNet <dietodeath23@gmail.com>
2015-04-08 18:08:13 -0400

    Updated the README file and moved the sprite sheet and SuperCop logo to Images folder. Files have been updated correctly.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SuperCop
+Spring 2015 C++ Final Project
+
+Game Title: SuperCop
+Game Type: 2-D Side-scroller. (Original Super Mario style)
+
+Features:
+	-Three Difficulties
+	-"Endless" Mode [?]
+	-AI
+	-1 and 2 Player modes
--- SuperCop/mainwindow.cpp
+++ SuperCop/mainwindow.cpp
@@ -9,7 +9,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QPixmap bkgnd("../SuperCop/SuperCop.png");
+    QPixmap bkgnd("../SuperCop/Images/SuperCop.png");
         bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
         QPalette palette;
         palette.setBrush(QPalette::Background, bkgnd);
